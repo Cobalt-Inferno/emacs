baaf0a3a71bbd874ebb8a1570d81e69619da20d6
art <art>
2022-09-30 23:53:14 -0400

    Added some new features

--- config.el
+++ config.el
@@ -1,5 +1,6 @@
 ;; Require blocks
 (require 'evil)
+(require 'yasnippet)
 (require 'general)
 (require 'beacon)
 (require 'tree-sitter)
@@ -50,29 +51,20 @@
 (evil-ex-define-cmd "q" 'quit-buffer)
 ;; Set the title
 ;; Set the banner
-;; Value can be
-;; 'official which displays the official emacs logo
-;; 'logo which displays an alternative emacs logo
-;; 1, 2 or 3 which displays one of the text banners
-;; "path/to/your/image.gif", "path/to/your/image.png" or "path/to/your/text.txt" which displays whatever gif/image/text you would prefer
 
-;; Content is not centered by default. To center, set
-
-;; To disable shortcut "jump" indicators for each section, set
-
-;; treesitter langs
-(tree-sitter-require 'cpp)
+;; smooth scrolling
+(setq sublimity-scroll-weight 5
+      sublimity-scroll-drift-length 5)
+;; for vim like tab
+(define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)
+	
+(yas-global-mode 1)
+ (tree-sitter-require 'cpp)
 (tree-sitter-require 'c)
 (global-tree-sitter-mode)
 (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
 ;; lsp
-(setq lsp-log-io nil) ; if set to true can cause a performance hit
-(with-eval-after-load 'lsp-mode
-  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration))
-(setq company-minimum-prefix-length 1
-      company-idle-delay 0.0) ;; default is 0.2
-
 ;; Autopair
 (defun electric-pair ()
       "If at end of line, insert character pair without surrounding spaces.
@@ -97,4 +89,4 @@
 (setq sublimity-scroll-weight 5
       sublimity-scroll-drift-length 5)
 ;; for vim like tab
-(define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)
+(define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)      
--- packages.el
+++ packages.el
@@ -120,28 +120,6 @@
   :config
   (smex-initialize))
 
-;; LSP
-(use-package lsp-mode
-  :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l")
-  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (clangd-mode . lsp)
-         ;; if you want which-key integration
-         (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
-
-;; optionally
-(use-package lsp-ui :commands lsp-ui-mode)
-;; if you are helm user
-(use-package helm-lsp :commands helm-lsp-workspace-symbol)
-;; if you are ivy user
-(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-
-;; optionally if you want to use debugger
-(use-package dap-mode)
-;; (use-package dap-LANGUAGE) to load the dap adapter for your language
 
 ;; optional if you want which-key integration
 (use-package which-key
@@ -152,3 +130,17 @@
   :ensure t
   :config
   (sublimity-mode 1))
+;; for C/C++
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode t))
+(use-package yasnippet
+  :ensure t)
